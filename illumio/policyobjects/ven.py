9aca636e003f600a9c2c1eafa28e8d7692f53b1a
Duncan Sommerville <duncan.sommerville@illumio.com>
2021-11-10 03:37:08 -0500

    flesh out workload object definition and subclasses

---
+++ illumio/policyobjects/ven.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from . import (
+    JsonObject,
+    UserObject
+)
+
+
+@dataclass
+class AgentConfig(JsonObject):
+    mode: str = None
+    log_traffic: bool = None
+    security_policy_update_mode: str = None
+
+
+@dataclass
+class SecureConnect(JsonObject):
+    matching_issuer_name: str = None
+
+
+@dataclass
+class AgentHealthErrors(JsonObject):
+    errors: List[str] = field(default_factory=list)
+    warnings: List[str] = field(default_factory=list)
+
+
+@dataclass
+class AgentHealth(JsonObject):
+    type: str = None
+    severity: str = None
+    audit_event: str = None
+
+
+@dataclass
+class AgentStatus(JsonObject):
+        uid: str = None
+        last_heartbeat_on: str = None
+        uptime_seconds: int = None
+        agent_version: str = None
+        managed_since: str = None
+        fw_config_current: bool = None
+        firewall_rule_count: int = None
+        security_policy_refresh_at: str = None
+        security_policy_applied_at: str = None
+        security_policy_received_at: str = None
+        agent_health_errors: AgentHealthErrors = None
+        agent_health: List[AgentHealth] = None
+        security_policy_sync_state: str = None
+
+        def _decode_complex_types(self) -> None:
+            if self.agent_health_errors:
+                self.agent_health_errors = AgentHealthErrors.from_json(self.agent_health_errors)
+            self.agent_health = [AgentHealth.from_json(o) for o in self.agent_health] if self.agent_health else None
+
+
+@dataclass
+class VENAgent(UserObject):
+    config: AgentConfig = None
+    secure_connect: SecureConnect = None
+    status: AgentStatus = None
+    active_pce_fqdn: str = None
+    target_pce_fqdn: str = None
+    type: str = None
+
+    def _decode_complex_types(self):
+        self.config = AgentConfig.from_json(self.config) if self.config else None
+        self.secure_connect = SecureConnect.from_json(self.secure_connect) if self.secure_connect else None
+        self.status = AgentStatus.from_json(self.status) if self.status else None
+
+
+@dataclass
+class VEN(UserObject):
+    hostname: str = None
+    name: str = None
+    status: str = None
--- illumio/policyobjects/workload.py
+++ illumio/policyobjects/workload.py
@@ -1,8 +1,168 @@
 from dataclasses import dataclass
+from typing import List
 
-from .user import UserObject
+from illumio import IllumioException, IllumioEnum
+
+from . import (
+    JsonObject,
+    UserObject,
+    ContainerCluster,
+    Label,
+    Network,
+    Service,
+    VENAgent,
+    VEN,
+    Vulnerability,
+    VulnerabilityReport
+)
+
+
+class LinkState(IllumioEnum):
+    UP = 'up'
+    DOWN = 'down'
+    UNKNOWN = 'unknown'
+
+
+class Mode(IllumioEnum):
+    IDLE = 'idle'
+    ILLUMINATED = 'illuminated'
+    ENFORCED = 'enforced'
+
+
+class EnforcementMode(IllumioEnum):
+    IDLE = 'idle'
+    VISIBILITY_ONLY = 'visibility_only'
+    FULL = 'full'
+    SELECTIVE = 'selective'
+
+
+@dataclass
+class Interface(JsonObject):
+    name: str
+    link_state: str = None
+    address: str = None
+    cidr_block: int = None
+    default_gateway_address: str = None
+    network: Network = None
+    network_detection_mode: str = None
+    friendly_name: str = None
+
+    def _validate(self):
+        if self.link_state and not LinkState.has_value(self.link_state):
+            raise IllumioException("Invalid link_state: {}".format(self.link_state))
+
+    def _decode_complex_types(self):
+        self.network = Network.from_json(self.network) if self.network else None
+
+
+@dataclass
+class WorkloadService(JsonObject):
+    uptime_seconds: int = None
+    created_at: int = None
+    open_service_ports: List[Service] = None
+
+    def _decode_complex_types(self):
+        self.open_service_ports = [Service.from_json(o) for o in self.open_service_ports]
+
+
+@dataclass
+class PortWideExposure(JsonObject):
+    any: bool = None
+    ip_list: bool = None
+
+
+@dataclass
+class VulnerabilitiesSummary(JsonObject):
+    num_vulnerabilities: int = None
+    vulnerable_port_exposure: int = None
+    vulnerable_port_wide_exposure: PortWideExposure = None
+    vulnerability_exposure_score: int = None
+    vulnerability_score: int = None
+    max_vulnerability_score: int = None
+
+    def _decode_complex_types(self):
+        if self.vulnerable_port_wide_exposure:
+            self.vulnerable_port_wide_exposure = PortWideExposure.from_json(self.vulnerable_port_wide_exposure)
+
+
+@dataclass
+class DetectedVulnerability(JsonObject):
+    ip_address: str = None
+    port: int = None
+    proto: int = None
+    port_exposure: int = None
+    port_wide_exposure: PortWideExposure = None
+    workload: 'Workload' = None
+    vulnerability: Vulnerability = None
+    vulnerability_report: VulnerabilityReport = None
+
+    def _decode_complex_types(self):
+        if self.port_wide_exposure:
+            self.port_wide_exposure = PortWideExposure.from_json(self.port_wide_exposure)
+        self.workload = Workload(href=self.workload['href']) if self.workload else None
+        self.vulnerability = Vulnerability.from_json(self.vulnerability) if self.vulnerability else None
+        if self.vulnerability_report:
+            self.vulnerability_report = VulnerabilityReport.from_json(self.vulnerability_report)
+
+
+@dataclass
+class IKEAuthenticationCertificate(JsonObject):
+    pass
 
 
 @dataclass
 class Workload(UserObject):
-    pass
+    hostname: str = None
+    os_type: str = None
+    service_principal_name: str = None
+    agent_to_pce_certificate_authentication_id: str = None
+    distinguished_name: str = None
+    public_ip: str = None
+    external_data_set: str = None
+    external_data_reference: str = None
+    interfaces: List[Interface] = None
+    service_provider: str = None
+    data_center: str = None
+    data_center_zone: str = None
+    os_id: str = None
+    os_detail: str = None
+    online: bool = None
+    firewall_coexistence: str = None
+    containers_inherit_host_policy: bool = None
+    blocked_connection_action: str = None
+    labels: List[Label] = None
+    services: List[WorkloadService] = None
+    vulnerabilities_summary: VulnerabilitiesSummary = None
+    detected_vulnerabilities: List[DetectedVulnerability] = None
+    agent: VENAgent = None
+    ven: VEN = None
+    enforcement_mode: str = None
+    selectively_enforced_services: List[Service] = None
+    container_cluster: ContainerCluster = None
+    ike_authentication_certificate: IKEAuthenticationCertificate = None
+
+    def _validate(self):
+        if self.enforcement_mode and not EnforcementMode.has_value(self.enforcement_mode):
+            raise IllumioException("Invalid enforcement_mode: {}".format(self.enforcement_mode))
+
+    def _decode_complex_types(self):
+        if self.interfaces:
+            self.interfaces = [Interface.from_json(o) for o in self.interfaces]
+        if self.labels:
+            self.labels = [Label.from_json(o) for o in self.labels]
+        if self.services:
+            self.services = [WorkloadService.from_json(o) for o in self.services]
+        if self.vulnerabilities_summary:
+            self.vulnerabilities_summary = VulnerabilitiesSummary.from_json(self.vulnerabilities_summary)
+        if self.detected_vulnerabilities:
+            self.detected_vulnerabilities = [DetectedVulnerability.from_json(o) for o in self.detected_vulnerabilities]
+        if self.agent:
+            self.agent = VENAgent.from_json(self.agent)
+        if self.ven:
+            self.ven = VEN.from_json(self.ven)
+        if self.selectively_enforced_services:
+            self.selectively_enforced_services = [Service.from_json(o) for o in self.selectively_enforced_services]
+        if self.container_cluster:
+            self.container_cluster = ContainerCluster.from_json(self.container_cluster)
+        if self.ike_authentication_certificate:
+            self.ike_authentication_certificate = IKEAuthenticationCertificate.from_json(self.ike_authentication_certificate)
