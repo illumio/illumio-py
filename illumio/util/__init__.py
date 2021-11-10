0c33c6d00a1cee67436726357b21a1b7ca95e212
Duncan Sommerville <duncan.sommerville@illumio.com>
2021-11-10 03:02:09 -0500

    define extendable base enum class for package-wide use

--- illumio/util/__init__.py
+++ illumio/util/__init__.py
@@ -1,2 +1,11 @@
+from enum import Enum
+
 from .functions import *
 from .jsonutils import *
+
+
+class IllumioEnum(Enum):
+
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_
