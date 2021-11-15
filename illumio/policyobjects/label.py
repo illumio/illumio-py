0720ac4c4c0fc6d8979f9f70ed11811459c431ad
Duncan Sommerville <duncan.sommerville@illumio.com>
2021-11-15 08:31:42 -0500

    add LabelSet object type

--- illumio/policyobjects/label.py
+++ illumio/policyobjects/label.py
@@ -1,7 +1,9 @@
+import json
 from dataclasses import dataclass
 from typing import List
 
 from illumio.accessmanagement import UserObject
+from illumio.util.jsonutils import JsonObject
 
 
 @dataclass
@@ -22,3 +24,24 @@ class LabelGroup(Label):
     def _decode_complex_types(self) -> None:
         super()._decode_complex_types()
         self.labels = [Label.from_json(o) for o in self.labels]
+
+
+@dataclass
+class LabelSet(JsonObject):
+    labels: List[Label] = None
+
+    def to_json(self):
+        return [{'label_group' if type(label) is LabelGroup else 'label': label.to_json()} for label in self.labels]
+
+    @classmethod
+    def from_json(cls, data) -> 'LabelSet':
+        data = json.loads(data) if type(data) is str else data
+        labels = []
+        for label_entry in data:
+            key = 'label'
+            label_type = Label
+            if key not in label_entry:
+                key = 'label_group'
+                label_type = LabelGroup
+            labels.append(label_type.from_json(label_entry[key]))
+        return LabelSet(labels=labels)
