61f1da45d7cda9fc86ebf131bd8b3e08e47f9343
Duncan Sommerville <duncan.sommerville@illumio.com>
2021-11-15 10:29:14 -0500

    fix PCE functions to send objects rather than JSON strings; provide more detailed error messages in case of API exceptions

--- illumio/pce.py
+++ illumio/pce.py
@@ -29,7 +29,12 @@ class PolicyComputeEngine:
             response.raise_for_status()
             return response
         except Exception as e:
-            raise IllumioApiException from e
+            message = str(e)
+            if response is not None and response.content:
+                message = "API call returned error code {}. Errors:".format(response.status_code)
+                for error in response.json():
+                    message += '\n{}: {}'.format(error['token'], error['message'])
+            raise IllumioApiException(message) from e
 
     def _set_request_headers(self, is_async=False, **kwargs):
         headers = kwargs.get('headers', {})
@@ -87,19 +92,24 @@ class PolicyComputeEngine:
         return VirtualService.from_json(response.json())
 
     def create_virtual_service(self, virtual_service: VirtualService, **kwargs) -> VirtualService:
-        kwargs['json'] = json.dumps(virtual_service, cls=IllumioEncoder)
+        kwargs['json'] = virtual_service.to_json()
         response = self.post('/sec_policy/draft/virtual_services', **kwargs)
         return VirtualService.from_json(response.json())
 
     def get_traffic_flows(self, traffic_query: TrafficQuery, **kwargs) -> List[TrafficFlow]:
-        kwargs['json'] = json.dumps(traffic_query, cls=IllumioEncoder)
+        kwargs['json'] = traffic_query.to_json()
         response = self.post('/traffic_flows/traffic_analysis_queries', **kwargs)
         return [TrafficFlow.from_json(flow) for flow in response.json()]
 
     def get_traffic_flows_async(self, query_name: str, traffic_query: TrafficQuery, **kwargs) -> List[TrafficFlow]:
+        # the redundancy/reuse between this function and get_collection is unfortunately necessary due
+        # to the completely different request & response structure for the async calls. note how
+        # location is pulled from the initial response HREF rather than a header, retry-after is missing,
+        # the success status is 'completed' rather than 'done', and the 'result' value of the response
+        # contains the HREF directly rather than an object
         try:
             traffic_query.query_name = query_name
-            kwargs['json'] = json.dumps(traffic_query, cls=IllumioEncoder)
+            kwargs['json'] = traffic_query.to_json()
             self._set_request_headers(is_async=True, **kwargs)
             response = self._session.post(self._build_url('/traffic_flows/async_queries'), **kwargs)
             response.raise_for_status()
@@ -129,6 +139,5 @@ class PolicyComputeEngine:
 
     def provision_policy_changes(self, change_description: str, hrefs: List[str], **kwargs) -> None:
         policy_changeset = PolicyChangeset.build(hrefs)
-        policy_json = {'update_description': change_description, 'change_subset': policy_changeset}
-        kwargs['json'] = json.dumps(policy_json, cls=IllumioEncoder)
+        kwargs['json'] = {'update_description': change_description, 'change_subset': policy_changeset.to_json()}
         self.post('/sec_policy', **kwargs)
