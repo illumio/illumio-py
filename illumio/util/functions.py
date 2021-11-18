8a333b8d996301603b8c0e5d2dfc1afcb5dc276e
Duncan Sommerville <duncan.sommerville@illumio.com>
2021-11-18 10:36:18 -0500

    add helper function to convert draft href to active

--- illumio/util/functions.py
+++ illumio/util/functions.py
@@ -1,5 +1,7 @@
 import socket
 
+from .constants import ACTIVE, DRAFT
+
 
 def convert_protocol(protocol: str) -> int:
     return socket.getprotobyname(protocol)
@@ -7,3 +9,7 @@ def convert_protocol(protocol: str) -> int:
 
 def ignore_empty_keys(o):
     return {k: v for (k, v) in o if v is not None}
+
+
+def convert_draft_href_to_active(href: str) -> str:
+    return href.replace('/{}/'.format(DRAFT), '/{}/'.format(ACTIVE))
