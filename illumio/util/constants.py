829f630bedef041ca8cd8472b9eb7460f48fc0aa
Duncan Sommerville <duncan.sommerville@illumio.com>
2021-11-18 10:35:54 -0500

    move base classes to jsonutils module to avoid circular refs

--- illumio/util/constants.py
+++ illumio/util/constants.py
@@ -1,9 +1,5 @@
 import re
-from dataclasses import dataclass
 from enum import Enum
-from typing import List
-
-from .jsonutils import JsonObject
 
 ACTIVE = 'active'
 DRAFT = 'draft'
@@ -11,39 +7,7 @@ DRAFT = 'draft'
 ANY_IP_LIST_NAME = 'Any (0.0.0.0/0 and ::/0)'
 
 FQDN_REGEX = re.compile('(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)')
-POLICY_OBJECT_HREF_REGEX = re.compile('^\/orgs\/\d+\/sec_policy\/(?:active|draft)\/(?P<type>[a-zA-Z_]+)\/(?P<uid>[a-zA-Z0-9-]+)$')
-
-
-@dataclass
-class Reference(JsonObject):
-    href: str = None
-
-
-@dataclass
-class IllumioObject(JsonObject):
-    href: str = None
-    name: str = None
-    description: str = None
-    external_data_set: str = None
-    external_data_reference: str = None
-
-
-@dataclass
-class ModifiableObject(IllumioObject):
-    created_at: str = None
-    updated_at: str = None
-    deleted_at: str = None
-    update_type: str = None
-    delete_type: str = None
-    created_by: Reference = None
-    updated_by: Reference = None
-    deleted_by: Reference = None
-    caps: List[str] = None
-
-    def _decode_complex_types(self) -> None:
-        self.created_by = Reference(href=self.created_by) if self.created_by else None
-        self.updated_by = Reference(href=self.updated_by) if self.updated_by else None
-        self.deleted_by = Reference(href=self.deleted_by) if self.deleted_by else None
+HREF_REGEX = re.compile('^\/orgs\/\d+\/(?:sec_policy\/(?:active|draft)\/)?(?P<type>[a-zA-Z_]+)\/(?P<uid>[a-zA-Z0-9-]+)$')
 
 
 class IllumioEnum(Enum):
--- illumio/util/jsonutils.py
+++ illumio/util/jsonutils.py
@@ -1,7 +1,7 @@
 import json
 from abc import ABC
 from dataclasses import dataclass, asdict
-from typing import Any
+from typing import List, Any
 
 from .functions import ignore_empty_keys
 
@@ -34,3 +34,35 @@ class JsonObject(ABC):
         o = cls(**data)
         o._decode_complex_types()
         return o
+
+
+@dataclass
+class Reference(JsonObject):
+    href: str = None
+
+
+@dataclass
+class IllumioObject(JsonObject):
+    href: str = None
+    name: str = None
+    description: str = None
+    external_data_set: str = None
+    external_data_reference: str = None
+
+
+@dataclass
+class ModifiableObject(IllumioObject):
+    created_at: str = None
+    updated_at: str = None
+    deleted_at: str = None
+    update_type: str = None
+    delete_type: str = None
+    created_by: Reference = None
+    updated_by: Reference = None
+    deleted_by: Reference = None
+    caps: List[str] = None
+
+    def _decode_complex_types(self) -> None:
+        self.created_by = Reference(href=self.created_by) if self.created_by else None
+        self.updated_by = Reference(href=self.updated_by) if self.updated_by else None
+        self.deleted_by = Reference(href=self.deleted_by) if self.deleted_by else None
