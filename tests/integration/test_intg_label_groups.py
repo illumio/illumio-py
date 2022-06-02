3818743cac2173465bf465d77600d73b8a6fccf8
Duncan Sommerville <duncan.sommerville@illumio.com>
2022-06-02 03:03:10 -0400

    add LabelGroup integration tests

---
+++ tests/integration/test_intg_label_groups.py
@@ -0,0 +1,56 @@
+import pytest
+
+from illumio.policyobjects import LabelGroup
+
+from helpers import random_string
+
+
+@pytest.fixture
+def label_group(pce, session_identifier, role_label):
+    identifier = random_string()
+    label_group = pce.label_groups.create(
+        LabelGroup(
+            key='role',
+            name='{}-LG-{}'.format(session_identifier, identifier),
+            description='Created by illumio python library integration tests',
+            labels=[role_label],
+            external_data_set=session_identifier,
+            external_data_reference=identifier
+        )
+    )
+    yield label_group
+    pce.label_groups.delete(label_group.href)
+
+
+def test_get_by_href(pce, label_group):
+    lg = pce.label_groups.get_by_href(label_group.href)
+    assert lg == label_group
+
+
+def test_get_by_key(pce, session_identifier, label_group):
+    label_groups = pce.label_groups.get(params={'key': 'role', 'name': session_identifier})
+    assert len(label_groups) == 1
+
+
+def test_get_async(pce, session_identifier, label_group):
+    label_groups = pce.label_groups.get_async(params={'name': session_identifier})
+    assert len(label_groups) == 1
+
+
+def test_add_label_to_group(pce, session_identifier, label_group):
+    identifier = random_string()
+    role_label = pce.labels.create({
+        'key': 'role',
+        'value': '{}-R-{}'.format(session_identifier, identifier),
+        'external_data_set': session_identifier,
+        'external_data_reference': identifier
+    })
+
+    label_group.labels.append(role_label)
+    print(label_group.labels)
+    pce.label_groups.update(label_group.href, {
+        'labels': label_group.labels
+    })
+
+    lg = pce.label_groups.get_by_href(label_group.href)
+    assert len(lg.labels) == 2
