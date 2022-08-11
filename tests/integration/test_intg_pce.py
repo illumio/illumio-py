fb953c4fbe6abfa55191223dfd27523ba044a94c
Duncan Sommerville <duncan.sommerville@illumio.com>
2022-08-11 12:41:47 -0400

    add invalid org_id connection test

--- tests/integration/test_intg_pce.py
+++ tests/integration/test_intg_pce.py
@@ -3,3 +3,12 @@ from illumio import PolicyComputeEngine
 
 def test_pce_connection(pce: PolicyComputeEngine):
     assert pce.check_connection()
+
+
+def test_invalid_org_id(pce: PolicyComputeEngine):
+    # since pce is a session fixture, store the original id
+    # to reset after we test the failed connection request
+    configured_org_id = pce.org_id
+    pce.org_id = 999
+    assert not pce.check_connection()
+    pce.org_id = configured_org_id
